3ff11e1bf169601c4d31a2b9ee4d66fea31f8c14
aarburu <aarburuzu25bj@ikzubirimanteo.com>
2025-11-19 13:39:38 +0100

    Feature player controls (#5)
    * Player Controller.
    
    Initial PlayerController system.
    
    * Player controls refactor
    
    * PlayerPrefab adjustments.
    
    * Added .vsconfig to .gitignore
    
    * Merge with develop
    
    Resolved conflicts
    Locked and hide cursor
    
    * Missing merge
    
    * Ledge grab
    
    Added ledge grab system.
    Extracted configuration values into PlayerConfig and dedicated config files.
    
    * Input handling refactorization.
    
    * Adjust PlayerConfig property visibility
    
    * Remove unused PlayerInputHandler class

---
+++ Assets/Scripts/Player/Handlers/PlayerCrouchHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerCrouchHandler
+{
+    private readonly CrouchConfig config;
+
+    public PlayerCrouchHandler (CrouchConfig config)
+    {
+        this.config = config;
+    }
+
+    public float GetTargetHeight (PlayerState state, float currentHeight)
+    {
+        float targetHeight = state == PlayerState.Crouching ? config.crouchingHeight : config.standingHeight;
+        return Mathf.Lerp(currentHeight, targetHeight, Time.deltaTime * config.transitionSpeed);
+    }
+}
---
+++ Assets/Scripts/Player/Handlers/PlayerPhysicsHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerPhysicsHandler
+{
+    private readonly PhysicsConfig config;
+
+    public PlayerPhysicsHandler (PhysicsConfig config)
+    {
+        this.config = config;
+    }
+
+    public Vector3 ApplyGravity (PlayerState state, Vector3 velocity, bool isGrounded)
+    {
+        if (state == PlayerState.Swimming || state == PlayerState.Diving)
+            return new Vector3(velocity.x, 0, velocity.z);
+
+        if (isGrounded && velocity.y < 0)
+            return new Vector3(velocity.x, -2f, velocity.z);
+
+        return velocity + Vector3.up * config.gravity * Time.deltaTime;
+    }
+}
