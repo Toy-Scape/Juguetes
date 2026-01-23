78e4cd85a87a31b24f7b00c8d5b4b5dfacc928f4
aarburu <aarburuzu25bj@ikzubirimanteo.com>
2026-01-23 18:00:14 +0100

    Fix reset when item falls from checkpoint while touching deadzone.

--- Assets/Scripts/Checkpoint System/DeadZone.cs
+++ Assets/Scripts/Checkpoint System/DeadZone.cs
@@ -7,7 +7,21 @@ namespace CheckpointSystem
     {
         private void OnTriggerEnter(Collider other)
         {
-            // TryGetComponent es más eficiente que GetComponent.
+            HandleTrigger(other);
+        }
+
+        // Un item que esté tocando el checkpoint no se va a resetear al entrar en el deadzone.
+        //  Si este objeto que está tocando el deadzone se cae de alguna forma del checkpoint,
+        //  no va a accionar el deadzone porque ya habia entrado. 
+        // Añado el OnTriggerStay para corregirlo.
+        private void OnTriggerStay(Collider other)
+        {
+            HandleTrigger(other);
+        }
+
+        private void HandleTrigger(Collider other)
+        {
+            // Player
             if (other.TryGetComponent<PlayerController>(out _) ||
                 other.GetComponentInParent<PlayerController>() != null)
             {
@@ -15,10 +29,15 @@ namespace CheckpointSystem
                 return;
             }
 
+            // Resettable
             var resettable = other.GetComponentInParent<IResettable>();
             if (resettable != null)
             {
-                if (resettable is ResettableItem item && item.IsTouchingCheckpoint) return;
+                // Si es un item y sigue tocando el checkpoint - NO resetear
+                if (resettable is ResettableItem item && item.IsTouchingCheckpoint)
+                    return;
+
+                // Si NO está tocando el checkpoint - resetear
                 resettable.ResetState();
             }
         }
