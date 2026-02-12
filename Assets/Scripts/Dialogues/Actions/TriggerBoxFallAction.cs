dab6ef64a8c62e2d63a8f9572796d80d3817a2d1
aarburu <aarburuzu25bj@ikzubirimanteo.com>
2026-02-12 11:14:30 +0100

    Tutorial feedback fixes (#88)
    * Reimported box models, added colliders, modified tutorial route.
    
    * Tutorial changes on props and added checkpoint to table.
    
    * fix - Colliders on tutorial boxes

---
+++ Assets/Scripts/Dialogues/Actions/TriggerBoxFallAction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "TriggerBoxFallAction", menuName = "Dialogue/Actions/TriggerBoxFall")]
+public class TriggerBoxFallAction : ActionBase
+{
+    public Vector3 finalPosition;
+    public Quaternion finalRotation;
+
+    public override void Execute(DialogueContext context)
+    {
+        BoxWobbleAndFall box = FindFirstObjectByType<BoxWobbleAndFall>();
+        if (box != null)
+        {
+            box.TriggerFall(finalPosition, finalRotation);
+        }
+    }
+}
