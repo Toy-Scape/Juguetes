2c4cabf3d80370ba21c7d146f8a12cdc007a329b
aarburu <aarburuzu25bj@ikzubirimanteo.com>
2026-02-24 10:00:12 +0100

    Feature/fan toy drop (#99)
    * Reimported box models, added colliders, modified tutorial route.
    
    * Tutorial changes on props and added checkpoint to table.
    
    * fix - Colliders on tutorial boxes
    
    * cosas
    
    * a
    
    * Add Paper Stack
    
    * la puñetera escena
    
    * cosas
    
    * Add WindAffected Script to sticky toy
    
    ---------
    
    Co-authored-by: aarburu <231005137+aarburu@users.noreply.github.com>
    Co-authored-by: anechf <aane03@gmail.com>

---
+++ Assets/Scripts/Physics/Wind/WindAffected.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityRandom = UnityEngine.Random;
+
+[RequireComponent(typeof(Rigidbody))]
+public class WindAffected : MonoBehaviour
+{
+    public float windSensitivity = 1f;
+    public float liftFactor = 0f;
+    public float turbulence = 0f;
+
+    public bool hasBeenBlown = false;
+    public Action<WindAffected> OnBlown;
+
+    Rigidbody rb;
+
+    protected virtual void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public virtual void ApplyWind(Vector3 direction, float force)
+    {
+        Vector3 finalForce = direction * force * windSensitivity;
+        finalForce += Vector3.up * force * liftFactor;
+
+        if (turbulence > 0f)
+            finalForce += UnityRandom.insideUnitSphere * turbulence;
+
+        if (!hasBeenBlown)
+        {
+            hasBeenBlown = true;
+            OnBlown?.Invoke(this);
+        }
+
+        rb.AddForce(finalForce, ForceMode.Force);
+    }
+}
